ff904e7bb464fe43fb96f756fdb1ea7cefc65f67
Abhaygupta-1212 <abhay.ag121200@gmail.com>
2024-09-01 21:13:24 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,18 @@
+let input = document.querySelector("#inp");
+let text=document.querySelector(".text");
+
+function Add(){
+    if(input.value==""){
+        alert("please enter task")
+    }else{
+        let newEle=document.createElement("ul");
+        newEle.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        input.value="";
+        newEle.querySelector("i").addEventListener("click",remove);
+        function remove(){
+            newEle.remove();
+        }
+    }
+
+}
